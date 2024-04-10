ba9ef5ef6849bbe39e5d630945c6503b23d0cfa5
Ralph Gonsalves <85131323+ralphg1002@users.noreply.github.com>
2024-04-09 23:35:11 -0400

    Add files via upload

---
+++ testrds.session.sql
@@ -0,0 +1,19 @@
+-- CREATE TABLE weatherrobs(
+--     date INT,
+--     hour INT,
+--     site varchar(255),
+--     temp INT,
+--     windspeed INT,
+--     weather text
+-- );
+
+-- INSERT INTO weatherrobs
+-- VALUES
+-- ('11202022', '15', 'Buffalo', '17', '45', 'heavy snow and lightning'),
+-- ('11202022', '15', 'Cleveland', '19', '27', 'light snow'),
+-- ('11202022', '15', 'Chicago', '15', '34', 'partly sunny'),
+-- ('11202022', '15', 'Albany', '20', '24', 'flurries');
+
+SELECT * FROM weatherrobs;
+
+-- DROP TABLE weatherrobs;
