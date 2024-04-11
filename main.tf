83d2042ebe0017a08408a3f7e6d5fe69981bb756
ralphg1002 <ralphg1002@hotmail.com>
2024-04-11 19:22:34 -0400

    first attempt to create table in db in tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "base_db" {
-    identifier           = "demo-db-instance"
+    identifier           = "mysql-db"
     allocated_storage    = 20
     storage_type         = "gp2"
     engine               = "mysql"
@@ -12,4 +12,17 @@ resource "aws_db_instance" "base_db" {
     skip_final_snapshot  = true
     availability_zone    = "us-east-1a"
     publicly_accessible  = true
+
+    provisioner "local-exec" {
+    command = <<-EOT
+      sleep 600 # Wait for RDS instance to be available
+      mysql -h ${self.endpoint} -u admin -p${self.password} <<MYSQL_SCRIPT
+      CREATE DATABASE IF NOT EXISTS exampledb;
+      USE exampledb;
+      CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), email VARCHAR(50));
+      INSERT INTO users (name, email) VALUES ('John Doe', 'john.doe@example.com');
+      INSERT INTO users (name, email) VALUES ('Jane Doe', 'jane.doe@example.com');
+      MYSQL_SCRIPT
+    EOT
+  }
 }
