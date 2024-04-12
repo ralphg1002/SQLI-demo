6747ea1794284ae8f846ed04a29074cf8cd1c3d4
ralphg1002 <ralphg1002@hotmail.com>
2024-04-11 23:12:31 -0400

    modify security group

--- main.tf
+++ main.tf
@@ -42,24 +42,13 @@ resource "aws_security_group" "ec2_sg" {
             security_groups = []
             self = false
         },
-        {
-            description = "MYSQL/Aurora"
-            from_port   = 3306
-            to_port     = 3306
-            protocol    = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []
-            security_groups = []
-            self = false
-        },
     ]
     egress {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
+        ipv6_cidr_blocks = []
     }
 }
 
